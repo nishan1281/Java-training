34e12ea6e4861e56acfd216e7eb81e256fa4149b
nishan1281 <arif.nishan2011@gmail.com>
2018-12-08 17:39:53 +0600

    updates

--- collection/ArrayListObject/src/com/nishan/Main.java
+++ collection/ArrayListObject/src/com/nishan/Main.java
@@ -1,8 +1,11 @@
 package com.nishan;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Books> listObject = new ArrayList<Books>();
     }
 }
